e0ac4fb38f32ddc3b815ac19cbb0572c4ad93671
wudibin <781721794@qq.com>
2020-11-13 15:15:24 +0800

    [wu] font : finish F-jest homework

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -1,14 +1,19 @@
+import { verifyUsername } from "../verify";
 import { register } from "../user";
 
 jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
+    await expect(register("username", "password")).resolves.toEqual({});
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    verifyUsername.mockReturnValue(false);
+    const result = register("username", "password");
+    await expect(result).rejects.toThrow("wrong username or password");
   });
 });
